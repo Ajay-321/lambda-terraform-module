52bb8b0ffa507156ff5a7f3c7af7ae5fecbb5700
Ajay Kumar Yadav <ajay10795@gmail.com>
2025-08-24 11:49:58 +0530

    updated backend bucket

--- dev/us-east-1/backend.tf
+++ dev/us-east-1/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-nsq-dev"
-    key    = "cdc/us-east1/us-east1.tfstate"
+    bucket = "dev-test-bucket-43"
+    key    = "dev/us-east1/us-east1.tfstate"
     region = "us-east-1"
   }
 }
